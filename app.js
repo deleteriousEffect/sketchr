27a12d5cdc6d96e564df28edeade10e5875a1004
Hayley Swimelar <hswimelar@gmail.com>
2015-01-12 19:01:20 -0800

    improved appearance, add header and button

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 /*jslint browser: true*/ /*global  $*/
 $(document).ready(function() {
-    function gridCollumns (collumns) {
+    function gridRows (rows) {
     var i;
-        for (i = 0; i <= collumns; i += 1) {
-            $('.sketchpad').append('<div class="collumn"></div>');
+        for (i = 0; i <= rows; i += 1) {
+            $('.sketchpad').append('<div class="row"></div>');
         }
     }
     function gridCreate (size) {
     var i;
-        gridCollumns (size);
+        gridRows (size);
         for (i = 0; i <= size; i += 1) {
-            $('.collumn').append('<div class="grid-unit"></div>');
+            $('.row').append('<div class="grid-unit"></div>');
         }
     }
     $('.sketchpad').on('mouseenter', '.grid-unit', function() {
