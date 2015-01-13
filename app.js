79807cc208528f2cb69041214463ec7028d09dcf
Hayley Swimelar <hswimelar@gmail.com>
2015-01-12 22:18:02 -0800

    sketched divs become darker on mouseenter

--- app.js
+++ app.js
@@ -21,12 +21,31 @@ $(document).ready(function() {
     }
     gridCreate(100);
     $('.sketchpad').on('mouseenter', '.grid-unit', function() {
-        $(this).addClass('sketched');
-        $(this).css("background-color", "rgb(" + randomRgb() + "," + randomRgb() + "," + randomRgb() + ")");
+        var currentRbgString = '',
+            rgbArr = [],
+            darkerRed = '',
+            darkerBlue = '',
+            darkerGreen = '';
+        if ($(this).attr('class') !== 'grid-unit sketched') {
+            $(this).addClass('sketched');
+            console.log($(this).attr('class'));
+            $(this).css('background-color', 'rgb(' + randomRgb() + ',' + randomRgb() + ',' + randomRgb() + ')');
+        } else {
+           console.log( $(this).css('background-color'));
+           currentRbgString = $(this).css('background-color');
+           console.log(currentRbgString);
+           rgbArr = currentRbgString.match(/\d+/g, '');
+           console.log(rgbArr);
+           darkerRed = parseInt(rgbArr[0] - 25.5, 10); 
+           darkerBlue = parseInt(rgbArr[1] - 25.5, 10); 
+           darkerGreen = parseInt(rgbArr[2] - 25.5, 10); 
+           console.log(darkerRed, darkerBlue, darkerGreen);
+           $(this).css('background-color', 'rgb(' + darkerRed + ',' + darkerBlue + ',' + darkerGreen + ')');
+        }
     });
     $('.sketchpad').on('click', 'button', function() {
         $('.sketchpad').find('.grid-unit').removeClass('sketched');
-        var userSize = window.prompt("How large to you want the grid to be?");
+        var userSize = window.prompt('How large to you want the grid to be?');
         gridDestory();
         gridCreate(userSize);
     });
